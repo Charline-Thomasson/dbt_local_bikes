e0468268a6c2db983589368a707a3d615ee334ed
Charline_T <ct.ae.training@gmail.com>
2025-05-19 16:09:23 +0000

    update: added staging models and a schema.yml file to fill in later

---
+++ models/staging/stg_local_bike_customer.sql
@@ -0,0 +1,13 @@
+select
+    customer_id,
+    first_name,
+    last_name,
+    concat(first_name, " ", last_name) as full_name,
+    phone,
+    email,
+    street,
+    city,
+    state,
+    zip_code,
+    concat(street, ", ", city, ", ", state, ", ", zip_code) as full_address
+from {{ source("local_bike", "customer") }}
